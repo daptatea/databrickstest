f1533e38b2548d67ae6dea0f28ac49b5aa2a2c73
Yomi Lajide <yolajide@microsoft.com>
2025-02-27 17:43:02 -0500

    update deploy/cleanup script, debug

--- e2e_samples/unstructured_data/scripts/functionzipdeploy.sh
+++ e2e_samples/unstructured_data/scripts/functionzipdeploy.sh
@@ -25,28 +25,35 @@ tar --exclude="node_modules/azure-functions-core-tools/*" \
     --exclude="node_modules/@types/*" \
     --exclude="infra/*" -a -c -f ./deploy.zip ./*
 
-# Create the storage container
-az storage container create --name $FUNCTION_APP --account-name $STORAGE_ACCOUNT
+az functionapp deployment source config-zip -g $RESOURCE_GROUP -n $FUNCTION_APP --src ./deploy.zip
 
-# Upload zip file to Azure Blob Storage
-az storage blob upload \
-    --account-name $STORAGE_ACCOUNT \
-    --container-name $FUNCTION_APP \
-    --name $BLOB_NAME \
-    --file ./deploy.zip \
-    --overwrite
+# # Create the storage container
+# az storage container create --name $FUNCTION_APP --account-name $STORAGE_ACCOUNT
 
-# Construct the SAS URL
-BLOB_URL="https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}/${BLOB_NAME}"
+# # Upload zip file to Azure Blob Storage
+# az storage blob upload \
+#     --account-name $STORAGE_ACCOUNT \
+#     --container-name $FUNCTION_APP \
+#     --name $BLOB_NAME \
+#     --file ./deploy.zip \
+#     --overwrite
 
-echo "BLOB URL: $BLOB_URL"
+# # Construct the SAS URL
+# BLOB_URL="https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}/${BLOB_NAME}"
 
-# Set the WEBSITE_RUN_FROM_PACKAGE app setting to the deployment package SAS URL
-az functionapp config appsettings set -g $RESOURCE_GROUP -n $FUNCTION_APP --settings WEBSITE_RUN_FROM_PACKAGE=$BLOB_URL
+# echo "BLOB URL: $BLOB_URL"
 
-# Sync the function triggers
-az rest \
-    --method post \
-    --url https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Web/sites/$FUNCTION_APP/syncfunctiontriggers?api-version=2016-08-01
+# # Set the WEBSITE_RUN_FROM_PACKAGE app setting to the deployment package SAS URL
+# az functionapp config appsettings set -g $RESOURCE_GROUP -n $FUNCTION_APP --settings WEBSITE_RUN_FROM_PACKAGE=$BLOB_URL
 
-echo "Function App Deployment completed successfully."
+# # Sync the function triggers
+# az rest \
+#     --method post \
+#     --url "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Web/sites/$FUNCTION_APP/syncfunctiontriggers?api-version=2016-08-01"
+
+if [ $? -eq 0 ]; then
+    echo "Function App Deployment completed successfully."
+else
+    echo "Function App Zip Deployment failed"
+    exit 1
+fi
